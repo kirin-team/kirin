27d2130ada92f47050f4baa28457f68eb7471109
nemanjastanic <nemanja@d-one.design>
2023-08-03 14:22:44 +0200

    Fix: Remove Schema Folder

--- packages/db/grafbase/grafbase.config.ts
+++ packages/db/grafbase/grafbase.config.ts
@@ -1,3 +1,8 @@
 import { config, g } from '@grafbase/sdk';
 
+g.model('User', {
+  name: g.string(),
+  email: g.email(),
+});
+
 export default config({ schema: g });
--- packages/db/grafbase/schema/user.ts
+++
@@ -1,8 +0,0 @@
-import { GrafbaseSchema } from '@grafbase/sdk/dist/src/grafbase-schema';
-
-export default function user(g: GrafbaseSchema) {
-  g.model('User', {
-    name: g.string(),
-    email: g.email(),
-  });
-}
